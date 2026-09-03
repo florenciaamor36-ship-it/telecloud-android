b22775fe94cb1d8cf2b9d901c6a344e0b8b80e93
florenciaamor36-ship-it <florenciaamor36@gmail.com>
2026-09-03 15:46:03 -0300

    Publish app source

---
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ElegantDarkBg = Color(0xFF1A1C1E)
+val ElegantDarkSurface = Color(0xFF2F3033)
+val ElegantDarkSurfaceVariant = Color(0xFF1D1E21)
+val ElegantDarkPrimary = Color(0xFFD0E4FF)
+val ElegantDarkOnPrimary = Color(0xFF00315E)
+val ElegantDarkSecondary = Color(0xFFC2C6CF)
+val ElegantDarkTertiary = Color(0xFF909194)
+val ElegantDarkBorder = Color(0xFF44474E)
+val ElegantDarkText = Color(0xFFE2E2E6)
